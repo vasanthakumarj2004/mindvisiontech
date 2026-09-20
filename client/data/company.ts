5414411b1758e66130a7942a645c7e25cf65a357
prathap-32 <prathapguna32@gmail.com>
2026-09-20 14:39:29 +0530

    Address Modified

--- client/data/company.ts
+++ client/data/company.ts
@@ -9,7 +9,7 @@ export const companyInfo = {
     line1: "56, 472, Marudhamalai Road, P N Pudur,",
     line2: "Coimbatore, Tamil Nadu - 641041",
     full: "56, 472, Marudhamalai Road, P N Pudur, Coimbatore, Tamil Nadu - 641041",
-    city: "CBE",
+    city: "Coimbatore",
     state: "Tamil Nadu",
     pincode: "641041",
   },
